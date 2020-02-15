95c9ca9532a3525fab251654adec68e5a0d2338d
Aida Abdikulova <abdikulova@gmail.com>
2020-02-15 23:46:34 +0000

    Automatically backed up by Learn

--- app/application.rb
+++ app/application.rb
@@ -27,13 +27,14 @@ class Application
 
     elsif req.path.match(/add/)
       search_term = req.params["q"]
+      binding.pry
       if @@items.include?(search_term) == true
         @@cart << search_term
         resp.write "added #{search_term}"
       else
         resp.write "We don't have that item"
       end
-binding.pry
+
     else
       resp.write "Path Not Found"
     end
