94ed35f9b15d6fc825e5539ed559584e367ed5ac
Aida Abdikulova <abdikulova@gmail.com>
2020-02-15 23:16:49 +0000

    Automatically backed up by Learn

--- app/application.rb
+++ app/application.rb
@@ -16,7 +16,7 @@ class Application
       resp.write handle_search(search_term)
 
     elsif req.path.match(/cart/)
-      @@cart.empty? = true
+      @@cart.empty? 
       resp.write "Your cart is empty"
 
     else
