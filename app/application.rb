830bb68e7233b4d130c520f1175df17a56231214
Aida Abdikulova <abdikulova@gmail.com>
2020-02-15 23:34:56 +0000

    Automatically backed up by Learn

--- app/application.rb
+++ app/application.rb
@@ -29,9 +29,9 @@ class Application
       if @@items.include?(search_term)
         @@cart << search_term
         resp.write "added #{search_term}"
+        binding.pry
       else
         resp.write "We don't have that item"
-        binding.pry
       end
 
     else
